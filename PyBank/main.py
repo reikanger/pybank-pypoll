2fbe5a46832a80d3dcf694ccb77cc28527807d52
Ryan Eikanger <reikanger@gmail.com>
2024-04-11 14:20:45 -0500

    initial commit

---
+++ PyBank/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import os
+
+export_file = ""
+
+if __name__ == "__main__":
+    print("Financial Analysis")
+    print("----------------------------")
+    print("Total Months: ")
+    print("Total: ")
+    print("Average Change: $")
+    print("Greatest Increase in Profits: ($)")
+    print("Greatest Decrease in Profits: ($)")
