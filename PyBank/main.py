a9e8cdf6fbc0e71e8da90a6787b2ec45739b10f4
Ryan Eikanger <reikanger@gmail.com>
2024-04-14 20:59:47 -0500

    WIP updates to PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
+import csv
 import os
 
-export_file = ""
+export_file = os.path.join("analysis", "budget_analysis.csv")
+input_file = os.path.join("resources", "budget_data.csv") 
+
+months = []
 
 if __name__ == "__main__":
+
+
+    budget_data = {}
+
+    with open(input_file, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)  # skip header
+
+        for row in reader:
+            budget_data[row[0]] = row[1]
+
+    # total number of months included in dataset
+    total_months = len(budget_data.keys())
+
     print("Financial Analysis")
     print("----------------------------")
-    print("Total Months: ")
+    print(f"Total Months: {total_months}")
     print("Total: ")
     print("Average Change: $")
     print("Greatest Increase in Profits: ($)")
